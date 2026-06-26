f1178b0207db9770770bf6d0462849e821615548
Nachiket Girnar <nachiketdg@gmail.com>
2026-06-26 17:49:26 +0530

    Time: 1 ms (99.92%), Space: 46.7 MB (63.35%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,34 +9,64 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        //create minheap according to value 
-        //create a dummy node as unsure of the head 
-        PriorityQueue<ListNode> heap = new PriorityQueue<>((a,b)->a.val-b.val);
-        ListNode dummy = new ListNode(0);
+    public static ListNode merge(ListNode a, ListNode b)
+    {
+        ListNode dummy  = new ListNode(0);
         ListNode curr = dummy;
-        for(int i=0; i<lists.length; i++)
+        ListNode left = a;
+        ListNode right = b; 
+        while(left!=null && right!=null)
         {
-            if(lists[i] != null)
+            if(left.val<=right.val)
             {
-                heap.add(lists[i]);
+                curr.next = left;
+                curr = left;
+                left= left.next;
             }
-
-        }
-        while(!heap.isEmpty())
-        {
-            ListNode temp = heap.poll();
-            ListNode next = null;
-            if(temp.next!=null)
+            else
             {
-                next = temp.next;
-                heap.add(next);
-
+                curr.next = right;
+                curr = right;
+                right = right.next;
             }
-            curr.next = temp;
-            temp.next = null;
-            curr = temp;
         }
+        while(left!=null)
+            {
+                curr.next = left;
+                curr = left;
+                left= left.next;
+            }
+            while(right!=null)
+            {
+                curr.next = right;
+                curr = right;
+                right = right.next;
+            }
         return dummy.next;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0)
+        {
+            return null;
+        }
+        while(lists.length>1)
+        {
+            int count  = lists.length%2 == 0 ? lists.length/2 : (lists.length+1)/2;
+            ListNode [] res = new ListNode[count];
+            for(int i=0,j=0; i<lists.length;i+=2,j++)
+            {   
+                if(i<lists.length-1)
+                {
+                    res[j] = merge(lists[i],lists[i+1]);
+                }
+                else
+                {
+                    res[j] = lists[i];
+                }
+            }
+            lists = res;
+        }
+        
+        return lists[0];
     }
 }
