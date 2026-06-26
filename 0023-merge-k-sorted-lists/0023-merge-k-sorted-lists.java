867b1c106002994548617b0aac8e21d3175d0989
Nachiket Girnar <nachiketdg@gmail.com>
2026-06-26 17:50:30 +0530

    Time: 2 ms (90.17%), Space: 46.7 MB (78.23%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -70,3 +70,35 @@ class Solution {
         return lists[0];
     }
 }
+/*class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        //create minheap according to value 
+        //create a dummy node as unsure of the head 
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((a,b)->a.val-b.val);
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        for(int i=0; i<lists.length; i++)
+        {
+            if(lists[i] != null)
+            {
+                heap.add(lists[i]);
+            }
+
+        }
+        while(!heap.isEmpty())
+        {
+            ListNode temp = heap.poll();
+            ListNode next = null;
+            if(temp.next!=null)
+            {
+                next = temp.next;
+                heap.add(next);
+
+            }
+            curr.next = temp;
+            temp.next = null;
+            curr = temp;
+        }
+        return dummy.next;
+    }
+}*/
