6645aaf4c5c3b570aef6c22d78eb8c55d385be89
Nachiket Girnar <nachiketdg@gmail.com>
2026-06-26 16:04:15 +0530

    Time: 4 ms (63.49%), Space: 49.8 MB (99.46%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,6 +1,62 @@
 class Solution {
+    public static int[] merge(int[] a, int[] b)
+    {
+        int right = 0; 
+        int left = 0;
+        int res[] = new int[a.length+b.length];
+        int size =0;
+        while(left<a.length && right<b.length)
+        {
+            if(a[left]<=b[right])
+            {
+                res[size] = a[left];
+                size++;
+                left++;
+            }
+            else
+            {
+                res[size] = b[right];
+                size++;
+                right++;
+            }
+        }
+        while(left<a.length)
+            {
+                res[size] = a[left];
+                size++;
+                left++;
+            }
+            while(right<b.length)
+            {
+                res[size] = b[right];
+                size++;
+                right++;
+            }
+        return res;
+    }
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<int[]>heap = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        // merge pair wise 
+        while(matrix.length!=1)
+        {
+            int count = matrix.length%2 == 0 ? matrix.length/2 : (matrix.length+1)/2;
+            int[][] store = new int[count][];
+            for(int i=0,j=0; i<matrix.length; i+=2,j+=1)
+            {
+                if(i<matrix.length-1)
+                {
+                store[j] = merge(matrix[i],matrix[i+1]);
+                }
+                else
+                {
+                    store[j] = matrix[i];
+                }
+            }
+            matrix = store;
+        }
+        return matrix[0][k-1];
+    }
+}
+/*PriorityQueue<int[]>heap = new PriorityQueue<>((a,b)->a[0]-b[0]);
         for(int i=0; i<matrix.length;i++)
         {
             heap.add(new int[]{matrix[i][0],i,0});
@@ -24,6 +80,4 @@ class Solution {
                 heap.add(new int[]{matrix[list][index+1],list,index+1});
             }
         }
-        return arr[0];
-    }
-}
+        return arr[0];*/
