c43b405f60c50527ad225ec944e40ad8d2e59ffb
Nachiket Girnar <nachiketdg@gmail.com>
2026-06-28 01:50:25 +0530

    Time: 1784 ms (5.16%), Space: 201 MB (5.33%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -1,6 +1,76 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        List<int[]> list = new ArrayList<>();
+        //  1   2   3   5
+        //5 st
+        //3
+        //2
+        //1
+        //put first elemet in heap (arr[1]/arr[len-1])
+        // removing that add the next two best possible ans in place of that to the min heap;
+        //maintain a set so that no duplicate pair is present
+        // [numerartor_idx, denominator_idx]
+        PriorityQueue<int[]> heap = new PriorityQueue<>(
+    (a, b) -> Double.compare(
+        (double) arr[a[0]] / arr[a[1]],
+        (double) arr[b[0]] / arr[b[1]]
+    )
+);
+        Set <Pair<Integer,Integer>> set  = new HashSet<>();
+        heap.add((new int[]{0,arr.length-1}));
+        set.add(new Pair<>(0,arr.length-1));
+        int [] ans  = new int [2];
+        for(int i=0; i<k; i++)
+        {
+            int[] temp = heap.poll();
+            int num_idx = temp[0];
+            int den_idx = temp[1];
+            ans  = new int[]{arr[num_idx],arr[den_idx]};
+            if(!set.contains(new Pair<>(num_idx+1,den_idx)) && num_idx+1<arr.length)
+            {
+                heap.add(new int[]{num_idx+1,den_idx});
+                set.add(new Pair<>(num_idx+1,den_idx));
+            }
+            if(!set.contains(new Pair<>(num_idx,den_idx-1)) && den_idx-1>-1)
+            {
+                heap.add(new int[]{num_idx,den_idx-1});
+                set.add(new Pair<>(num_idx,den_idx-1));
+            }
+        }
+        return ans;
+        
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*List<int[]> list = new ArrayList<>();
         for(int i=0; i<arr.length;i++)
         {
             int a = arr[i];
@@ -12,6 +82,4 @@ class Solution {
         }
         Collections.sort(list, (x, y) ->
     Double.compare((double)x[0] / x[1], (double)y[0] / y[1]));
-        return list.get(k-1);
-    }
-}
+        return list.get(k-1); */
