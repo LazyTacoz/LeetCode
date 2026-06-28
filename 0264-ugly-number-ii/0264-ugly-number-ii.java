d4fc41dcbaf20bc39d83f539ee393e744313b30f
Nachiket Girnar <nachiketdg@gmail.com>
2026-06-28 23:06:40 +0530

    Time: 4 ms (32.31%), Space: 43.4 MB (49.1%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,6 +1,31 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        // min heap 
+       int[] dp = new int[n];
+       int[] idx = new int[3];
+       int[] primes = {2,3,5};
+       dp[0] = 1;
+       for(int i=1;i<n;i++)
+       {
+        int min = Integer.MAX_VALUE;
+        for(int j=0; j<primes.length; j++)
+        {
+            min = Math.min(min,dp[idx[j]]*primes[j]);
+        }
+        dp[i] = min;
+        for(int j=0; j<primes.length; j++)
+        {
+            if(min == dp[idx[j]]*primes[j])
+            {
+                idx[j]++;
+            }
+        }
+             
+       }
+    return dp[n-1];   
+    } 
+}
+/**
+ // min heap 
         //set for no duplis 
         // 1 is fix put in min heap
         // minheap.poll();
@@ -32,7 +57,4 @@ class Solution {
                 set.add(temp*5L);
             }
         }
-        return heap.peek().intValue();
-    
-    } 
-}
+        return heap.peek().intValue(); */
