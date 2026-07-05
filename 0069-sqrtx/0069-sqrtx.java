14c4e07520a856f22cf9471dfb63a9ced20ee4bf
Nachiket Girnar <nachiketdg@gmail.com>
2026-07-05 19:28:32 +0530

    Time: 1 ms (97.66%), Space: 42.5 MB (60.88%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,12 +1,41 @@
 class Solution {
     public int mySqrt(int x) {
-        int low = 1;
-        int high = x;
         // 1 2 3 4 5 6 7 8 9 10
         // T T T F F F F F F F 
         //FIND LAST TRUE 
+        //1 2 3 4 5 6 7 8 9 10
+        //l       m          h 
+        //l m   h            ans = 2  
+        //    l h           ans = 3
+        //      lh          
+        //    h l
+        if(x==1)
+        {
+            return 1;
+        }
+    int low = 1;
+    int high = x;
     int ans = 0;
-        while(low<=high)
+    //find first false
+    //return false-1;
+    while(low<high)
+    {
+        int mid = low+(high-low)/2;
+        if((long)mid*mid>x)
+        {
+            high = mid;
+        }
+        else
+        {
+            low = mid+1;
+        }
+    }
+        
+        return low-1;
+    }
+}
+/*
+while(low<=high)
         {
             int mid = low+(high-low)/2;
             if((long)mid*mid<=x)
@@ -20,6 +49,4 @@ class Solution {
             }
 
         }
-        return ans ;
-    }
-}
+        */
