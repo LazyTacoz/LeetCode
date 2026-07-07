5442ed4f4792fca6cb47e7254566972be0c2ab76
Nachiket Girnar <nachiketdg@gmail.com>
2026-07-07 11:26:51 +0530

    Time: 3 ms (2.54%), Space: 42.9 MB (69.72%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) != ' ') {
+
+                if (i == 0 || s.charAt(i - 1) == ' ') {
+                    count = 0;
+                }
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
