0820942002255600713d4c53a911d5653f96523d
Nachiket Girnar <nachiketdg@gmail.com>
2026-07-13 22:58:55 +0530

    Time: 25 ms (22.42%), Space: 48 MB (45.62%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -14,11 +14,9 @@ class Solution {
         // 1,2,3,4,5,6,7,8,9,10,11
         // F F F T T T T T T  T  T    CONDITION -> no*h<sum
         int max = 0;
-        int sum = 0;
         for(int i=0; i<piles.length; i++)
         {
             max = Math.max(max,piles[i]);
-            sum += piles[i];
         }
         int low = 1;
         int high = max;
