7aef5e91247dfd617acf8a28ed49a7c75d04e99b
Nachiket Girnar <nachiketdg@gmail.com>
2026-07-14 17:23:38 +0530

    Time: 1461 ms (5.17%), Space: 120.3 MB (6.81%) - LeetHub

---
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int i=0; i<flowers.length;i++)
+        {
+            map.put(flowers[i][0],map.getOrDefault(flowers[i][0],0)+1);
+            map.put(flowers[i][1]+1,map.getOrDefault(flowers[i][1]+1,0)-1);
+        }
+        int [][] flowersData = new int[map.size()][2];
+        int bloom_no = 0;
+        int i=0;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet())
+        {
+            bloom_no += entry.getValue();
+            flowersData[i] = new int[]{entry.getKey(),bloom_no};
+            i=i+1;
+        }
+        int [] res = new int[people.length];
+        for(int j=0; j<res.length;j++)
+        {
+            int time  = people[j];
+            if(time<flowersData[0][0])
+            {
+                res[j] = 0;
+                continue;
+            }
+            for(int k=1; k<flowersData.length;k++)
+            {
+                if(time == flowersData[k][0])
+                {
+                    res[j] = flowersData[k][1];
+                    break;
+                }
+                else if(time<flowersData[k][0])
+                {
+                    res[j] = flowersData[k-1][1];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
