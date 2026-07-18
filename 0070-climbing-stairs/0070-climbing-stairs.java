1f99cbe923658663ffc41cfb3c126904674ad50b
Nachiket Girnar <nachiketdg@gmail.com>
2026-07-18 20:06:41 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,17 @@
 class Solution {
-    int[] arr = new int[46];
+    Map<Integer,Integer> map = new HashMap<>();
     public int climbStairs(int n) {
         if(n==1 || n==0)
         {
             return 1;
         }
-        if(arr[n]!=0)
+        if(map.containsKey(n))
         {
-            return arr[n];
+            return map.get(n);
         }
         int a = climbStairs(n-1);
         int b = climbStairs(n-2);
-        arr[n] = a+b;
+        map.put(n,a+b);
         return a+b;
     }
 }
