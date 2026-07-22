2d9912253372c6685e6458f384f1f2d45e0879d2
Nachiket Girnar <nachiketdg@gmail.com>
2026-07-22 23:10:05 +0530

    Time: 2 ms (59.48%), Space: 47.1 MB (27.46%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -1,53 +1,88 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    Map<Integer,Integer> map = new HashMap<>();
-    public void helper(TreeNode root)
-    {
-        if(root == null) return;
-       helper(root.left);
-       map.put(root.val,map.getOrDefault(root.val,0)+1);
-       helper(root.right);
-    }
-    public int[] findMode(TreeNode root) 
-    {
-        helper(root);
-        int max = 0;
-       for(Map.Entry<Integer,Integer> entry : map.entrySet())
-       {
-            max = Math.max(max,entry.getValue());
-       }
-       List<Integer> list = new ArrayList<>();
 
-        for(Map.Entry<Integer,Integer> entry : map.entrySet())
+    int max = 0;
+    int count = 0;
+    Integer prev = null;
+
+    List<Integer> ans = new ArrayList<>();
+
+    public void inorder(TreeNode root)
+    {
+        TreeNode curr = root;
+
+        while(curr != null)
         {
-            if(entry.getValue() == max)
+            if(curr.left == null)
             {
-                list.add(entry.getKey());
+                process(curr.val);
+                curr = curr.right;
+            }
+            else
+            {
+                TreeNode ipre = ipre(curr);
+
+                if(ipre.right == null)
+                {
+                    ipre.right = curr;
+                    curr = curr.left;
+                }
+                else
+                {
+                    ipre.right = null;
+                    process(curr.val);
+                    curr = curr.right;
+                }
             }
         }
+    }
 
-        // Convert List<Integer> to int[]
-        int[] ans = new int[list.size()];
-
-        for(int i = 0; i < list.size(); i++)
+    public void process(int val)
+    {
+        if(prev == null || prev != val)
         {
-            ans[i] = list.get(i);
+            count = 1;
+            prev = val;
+        }
+        else
+        {
+            count++;
         }
 
-        return ans;
+        if(count > max)
+        {
+            max = count;
+            ans.clear();
+            ans.add(val);
+        }
+        else if(count == max)
+        {
+            ans.add(val);
+        }
+    }
+
+    public TreeNode ipre(TreeNode node)
+    {
+        TreeNode prev = node.left;
+
+        while(prev.right != null && prev.right != node)
+        {
+            prev = prev.right;
+        }
+
+        return prev;
+    }
+
+    public int[] findMode(TreeNode root)
+    {
+        inorder(root);
+
+        int[] res = new int[ans.size()];
+
+        for(int i = 0; i < ans.size(); i++)
+        {
+            res[i] = ans.get(i);
+        }
+
+        return res;
     }
 }
