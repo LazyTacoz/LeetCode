e235610f1da0ec513587fb7c088e971eb0a66f7d
Nachiket Girnar <nachiketdg@gmail.com>
2026-07-31 11:55:56 +0530

    Time: 2 ms (99.72%), Space: 45.2 MB (24.5%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<List<Integer>>ans  = new ArrayList<>();
+    public void helper(int[] nums,List<Integer>curr,int index)
+    {
+        ans.add(new ArrayList<>(curr));
+        
+        for(int i=index;i<nums.length;i++)
+        {
+            if(i > index && nums[i] == nums[i-1]) continue;
+            curr.add(nums[i]);
+            helper(nums,curr,i+1);
+            curr.remove(curr.size()-1);
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<Integer> curr = new ArrayList<>();
+        Arrays.sort(nums); 
+        helper(nums,curr,0);
+        return ans;
+    }
+}
